e5c6fae20d9a35ade1916e448af2383da52d8283
twinoa <ppangu2@gmail.com>
2023-10-06 12:40:26 +0900

    20231006 step6~18

--- dezero.py
+++ dezero.py
@@ -1,29 +1,114 @@
 import numpy as np
+import weakref
 
 class Variable:
     def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError('{}은(는) 지원하지 않습니다.'.format(type(data)))
+
         self.data = data
+        self.grad = None
+        self.creator = None
+        self.generation = 0  # 세대 수를 기록하는 변수
+    
+    def set_creator(self, func):
+        self.creator = func
+        self.generation = func.generation + 1  # 세대를 기록한다 (부모세대 + 1)
+
+    def backward(self, retain_grad=False):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = []
+        seen_set = set()
+
+        def add_func(f):
+            if f not in seen_set:
+                funcs.append(f)
+                seen_set.add(f)
+                funcs.sort(key=lambda x: x.generation)
+
+        add_func(self.creator)
+
+        while funcs:
+            f = funcs.pop()  # 1. 함수를 가져온다.
+            gys = [output().grad for output in f.outputs]
+            gxs = f.backward(*gys)
+            if not isinstance(gxs, tuple):
+                gxs = (gxs,)
+
+            for x, gx in zip(f.inputs, gxs):
+                if x.grad is None:
+                    x.grad = gx
+                else :
+                    x.grad = x.grad + gx
+
+                if x.creator is not None:
+                    add_func(x.creator)
+
+            if not retain_grad:
+                for y in f.outputs:
+                    y().grad = None  # y는 약한 참조 
+
+    def cleargrad(self):
+        self.grad = None
 
 
 class Function:
-    def __call__(self, input):
-        x = input.data  # 데이터를 꺼낸다.
-        y = self.forward(x)  # 구체적인 계산은 forward 메서드에서 한다.
-        output = Variable(y)  # Variable 형태로 되돌린다.
-        return output
+    def __call__(self, *inputs):
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs)  # 별표를 붙여 언팩
+        if not isinstance(ys, tuple):  # 튜플이 아닌 경우 추가 지원
+            ys = (ys,)
+        outputs = [Variable(as_array(y)) for y in ys]
+
+        self.generation = max([x.generation for x in inputs])
+        for output in outputs:
+            output.set_creator(self)
+        self.inputs = inputs
+        self.outputs = [weakref.ref(output) for output in outputs]
+
+        # 리스트의 원소가 하나라면 첫 번째 원소를 반환한다.
+        return outputs if len(outputs) > 1 else outputs[0]
     
+        
     def forward(self, x):
         raise NotImplementedError()
+    
+    def backward(self, gy):
+        raise NotImplementedError
+    
+
+class Add(Function):
+    def forward(self, x0, x1):
+        y = x0 + x1
+        return (y,)
+    
+    def backward(self, gy):
+        return gy, gy
     
 
 class Square(Function):
     def forward(self, x):
-        return x ** 2
+        y = x ** 2
+        return y
+    
+    def backward(self, gy):
+        x = self.inputs[0].data
+        gx = 2 * x * gy
+        return gx
     
 
 class Exp(Function):
     def forward(self, x):
-        return np.exp(x)
+        y = np.exp(x)
+        return y
+    
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
     
 
 def numerical_diff(f, x, eps=1e-4):
@@ -33,13 +118,27 @@ def numerical_diff(f, x, eps=1e-4):
     y1 = f(x1)
     return (y1.data - y0.data) / (2 * eps)
 
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
 
-def f(x):
-    A = Square()
-    B = Exp()
-    C = Square()
-    return C(B(A(x)))
+def square(x):
+    return Square()(x)  # 한 줄로 작성
 
-x = Variable(np.array(0.5))
-dy = numerical_diff(f, x)
-print(dy)
+def exp(x):
+    return Exp()(x)
+
+def add(x0, x1):
+    return Add()(x0, x1)
+
+
+
+x0 = Variable(np.array(1.0))
+x1 = Variable(np.array(1.0))
+t = add(x0, x1)
+y = add(x0, t)
+y.backward()
+
+print(y.grad, t.grad)
+print(x0.grad, x1.grad)
